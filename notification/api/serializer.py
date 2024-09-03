f1f591d39ee2e5abb2dcebe9e6509d69f45fab69
MANUEL ALEJANDRO <alejandro.mijangos@hotmail.com>
2024-09-02 23:28:34 -0600

    Before working on the API views

---
+++ notification/api/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+#
+# class MessageSentSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     category = serializers.CharField()
+#     description = serializers.CharField()
+#     active = serializers.BooleanField()
