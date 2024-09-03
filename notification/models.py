dafed929aa4308878560d63cf94516d87cb39654
MANUEL ALEJANDRO <alejandro.mijangos@hotmail.com>
2024-09-02 23:05:11 -0600

    Added a new model class for the logging of the sent messsages

--- notification/models.py
+++ notification/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+""" Django model class to store the Categories. It has a many to many relationship
+    with the BackendUser model class."""
+
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
@@ -12,12 +15,19 @@ class Category(models.Model):
         return self.name
 
 
+""" Django model class to store the Channels. It has a many to many relationship
+    with the BackendUser model class."""
+
+
 class Channel(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=254)
 
     def __str__(self):
         return self.name
+
+
+""" Django model class to store the Users."""
 
 
 class BackendUser(models.Model):
@@ -31,3 +41,17 @@ class BackendUser(models.Model):
         return f"{self.name}"
 
 
+""" Django model class to store the info for the messages sent (simulating the actual sending)."""
+
+
+class MessageSent(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    message_content = models.TextField()
+    # All the channels on which the message was sent
+    channels = models.ManyToManyField(Channel)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    # User id which can be used later to retrieve the rest of the info from the user Table
+    user = models.ForeignKey(BackendUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.sent_at} - {self.category} - {self.user}"
